50743a1590758def173968d911873e666a4ba83a
Kevin Dupeyrat <kevin.dupeyrat@gmail.com>
2018-03-22 15:29:56 +0100

    Add files via upload
    Initialisation

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Je suis l'Index";
+        }
+
+        public string Index2(int? value)
+        {
+            return "Je suis l'Index2 et mon paramètre est : " + value;
+        }
+    }
+}
